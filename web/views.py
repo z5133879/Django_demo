bf520a4d5303aa37633c149e4bacb234d9adf3cb
z5133879 <z5133879@users.noreply.github.com>
2019-11-03 15:17:26 +1100

    views.py change

--- web/views.py
+++ web/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render,redirect
 from django.core.exceptions import ObjectDoesNotExist
+
+from web import mongoDB
 from web.models import User,UserType
 from django.contrib.auth import authenticate,login
+from django.http import HttpResponse
 import re
+import pymongo
+from bson.objectid import ObjectId
+import json
+import random
 # Create your views here.
 def hello_view(request):
     return render(request, 'my-account.html',{
@@ -103,3 +110,173 @@ def search_content(request):
     return render(request,'search_content.html',{
 
     })
+
+def listing(request):
+
+    def get_connection():
+        client = pymongo.MongoClient('mongodb://47.74.87.45:27017/')
+        db = client['airtan']
+        collection = db['accomodation']
+        return client, db, collection
+
+    def query_doc_by_id(id):
+        mongodb, db, collection = get_connection()
+        doc = collection.find_one({'_id': ObjectId(id)})
+        print(doc)
+        return doc
+
+    def query_doc_by_index(query, page_size, page_num):
+        skip = page_size * (page_num - 1)
+        mongodb, db, collection = get_connection()
+        pages = collection.find(query).limit(page_size).skip(skip)
+
+        print(f'querying page {page_num}')
+        # for r in pages:
+        #     print(r)
+        return pages
+
+    def query_doc_by_name(name, page_size, page_num):
+        query = {"Name": {"$regex": name, "$options": "$i"}}
+        pages = query_doc_by_index(query, page_size, page_num)
+        return pages
+
+    def query_doc_by_location(location, page_size, page_num):
+        query = {"Location": {"$regex": location, "$options": "$i"}}
+        pages = query_doc_by_index(query, page_size, page_num)
+        # for r in pages:
+        #     print(r)
+        return pages
+
+    def insert_doc():
+        mongodb, db, collection = get_connection()
+
+    def delete_doc_by_id(id):
+        mongodb, db, collection = get_connection()
+        query = {'_id': ObjectId(id)}
+        x = collection.delete_many(query)
+        print(id)
+        print(x.deleted_count, "个文档已删除")
+
+    def query_doc():
+        mongodb, db, collection = get_connection()
+        dblist = collection.list_database_names()
+        if "airtan" in dblist:
+            print("airtan")
+        c = 0
+        for x in collection.find():
+            c += 1
+            print(x)
+        print(c)
+
+    abc = query_doc_by_location('coogee', 12, 1)
+    ddd = []
+    print("============")
+    #print(abc)
+    index = 0
+    for e in abc:
+        ##e.pop("_id")
+        e["page_id"] = "1"
+        e["number"] = str(index)
+        e["_id"] = str(e["_id"])
+        lat,lng = e["Coordinates"].split(",")
+        e["lat"] = float(lat)
+        e["lng"] = float(lng)
+        e["rating"] = round(random.uniform(3.0,5.0),1)
+        index += 1
+        print(e)
+        ddd.append(json.dumps(e))
+    print("dddddddedd")
+    print(ddd)
+
+    return render(request,'listing.html',{
+        'abc':json.dumps(ddd),
+        'page_id':"1",
+
+    })
+def more_listing(request,page_id='1'):
+    if page_id != "2":
+
+        a= mongoDB.MongoDb('coogee', 12, 1)
+        abc = a.get_result()
+    else:
+        a = mongoDB.MongoDb('coogee', 12, 2)
+        abc = a.get_result()
+    ddd = []
+    print("============")
+    #print(abc)
+    index = 0
+    for e in abc:
+        ##e.pop("_id")
+        e["page_id"] = "1"
+        e["number"] = str(index)
+        e["_id"] = str(e["_id"])
+        lat,lng = e["Coordinates"].split(",")
+        e["lat"] = float(lat)
+        e["lng"] = float(lng)
+        e["rating"] = round(random.uniform(3.0,5.0),1)
+        print(e["rating"])
+        index += 1
+        print(e)
+        ddd.append(json.dumps(e))
+    print("dddddddedd")
+    print(ddd)
+    if page_id == "undefined":
+        page_id = 1
+    return render(request, 'listing.html', {
+        'abc': json.dumps(ddd),
+        'page_id':page_id,
+
+    })
+
+    #return HttpResponse(page_id)
+def single_page(request):
+    id = request.GET.get('id')
+    page = request.GET.get('page')
+    mongon =     mongoDB.MongoDb('coogee', 12, int(page))
+    Json_data = mongon.get_result()
+    for e in Json_data:
+        if str(e["_id"]) == id:
+            e["O_id"] = id
+            lat, lng = e["Coordinates"].split(",")
+            e["lat"] = float(lat)
+            e["lng"] = float(lng)
+            e["page_id"] = page
+            e["rating"] = round(random.uniform(3.0, 5.0), 1)
+            Json = e
+            break
+
+    Photos = Json["Photos"]
+    photo_1 = Photos[0]
+    photo_2 = Photos[1]
+    photo_3 = Photos[2]
+    photo_4 = Photos[3]
+    photo_5 = Photos[4]
+    #print(page,id)
+    print(Photos)
+
+    return render(request,'listing-single.html',{
+        "json_date":Json,"photo_1":photo_1,"photo_2":photo_2,"photo_3":photo_3,"photo_4":photo_4,
+        "photo_5": photo_5,
+
+
+    })
+def dashboard(request):
+    return render(request,'dashboard.html')
+
+def dashboard_add(request):
+    return render(request,'dashboard-add-listing.html')
+
+def dashboard_myprofile(request):
+    return render(request,'dashboard-myprofile.html')
+
+def review(request):
+    return render(request,'dashboard-review.html')
+
+def easy_single(request):
+    return render(request,'listing-single2.html')
+
+def booking(request):
+    id = request.GET.get('id')
+    page = request.GET.get('page')
+    print(id,page)
+    return render(request,'booking-single.html')
